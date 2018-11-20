acbbb26bddf5a38d2d94041fdb676c7d69b005de
senggen <jackiecui@gmail.com>
2018-11-20 11:09:33 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-emulator -avd testAVD -wipe-data -noaudio -no-window -gpu off -verbose -qemu -vnc :2 -skin 1440x2560 &
+emulator -avd testAVD -wipe-data -noaudio -no-window -gpu off -verbose -qemu -vnc :2 &
 /usr/bin/dumb-init gitlab-runner run --user=root --working-directory=/home/gitlab-runner
